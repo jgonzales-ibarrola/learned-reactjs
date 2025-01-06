79b9e0c5f6dc1fb97f825ecc8bffaf5697780523
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-06 17:59:59 +0800

    feat: Learned Components & PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,59 @@
-import './App.css'
-import pokemon from '../pokemon.json'
+import "./App.css";
+import pokemon from "../pokemon.json";
+import PropTypes from "prop-types";
+
+function PokemonRow({ pokemon }) {
+
+	return (
+		<tr>
+			<td>{pokemon.name.english}</td>
+			<td>{pokemon.type.join(", ")}</td>
+		</tr>
+	);
+};
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }),
+    type: PropTypes.arrayOf(PropTypes.string)
+  })
+};
 
 function App() {
+	return (
+		<main>
+			<h1 className="title">Pokemon Search</h1>
 
-  return (
-    <main>
-      <h1 className='title'>Pokemon Search</h1>
-
-     <div 
-      style={{
-        margin: 'auto',
-        width: 800,
-        paddingTop: '1rem'
-      }}
-     >
-      <table width="100%">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {pokemon.slice(0, 20).map((pokemon) => {
-              const {id, base, name, type} = pokemon
-              const {chinese, english, french, japanese} = pokemon.name
-
-              return (
-                <tr key={[id, english].join(':')}>
-                  <td>{name.english}</td>
-                  <td>{type.join(", ")}</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-     </div>
-    </main>
-  )
+			<div
+				style={{
+					margin: "auto",
+					width: 800,
+					paddingTop: "1rem",
+				}}
+			>
+				<table width="100%">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Type</th>
+						</tr>
+					</thead>
+					<tbody>
+						{pokemon.slice(0, 20).map((pokemon) => (
+							<PokemonRow
+                pokemon={pokemon}
+								key={[pokemon.id, pokemon.name.english].join(
+									":"
+								)}
+							/>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</main>
+	);
 }
 
-export default App
+export default App;
