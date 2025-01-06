0b00f03e7e49374d9e90ca7068b475f3ac18ed79
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-06 21:32:04 +0800

    feat: Learned CSS in JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import PropTypes from "prop-types";
 import React from "react";
+import styled from "@emotion/styled";
 
 const PokemonRow = ({ pokemon, onSelect }) => {
 	return (
@@ -28,22 +29,22 @@ const PokemonInfo = ({ name, base }) => {
 	return (
 		<div>
 			<h1>{name.english}</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Key</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-       <tbody>
-        {Object.keys(base).map(key => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{base[key]}</td>
-            </tr>
-          ))}
-       </tbody>
-      </table>
+			<table>
+				<thead>
+					<tr>
+						<th>Key</th>
+						<th>Value</th>
+					</tr>
+				</thead>
+				<tbody>
+					{Object.keys(base).map((key) => (
+						<tr key={key}>
+							<td>{key}</td>
+							<td>{base[key]}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 };
@@ -62,69 +63,84 @@ PokemonInfo.propTypes = {
 	}),
 };
 
+const Title = styled.h1`
+	text-align: center;
+`;
+const TwoColumnsRow = styled.div`
+	display: grid;
+	grid-template-columns: 70% 30%;
+	grid-column-gap: 1rem;
+`;
+const Input = styled.input`
+	width: 100%;
+  font-size: x-large;
+  padding: 0.2rem;
+`;
+const Container = styled.div`
+	width: 100%;
+	margin: 20px auto;
+	padding: 20px 40px;
+`
+
 function App() {
 	const [filter, setFilter] = React.useState("");
 	const [selectedItem, setSelectedItem] = React.useState(null);
 	const [pokemon, setPokemon] = React.useState([]);
 
 	React.useEffect(() => {
-		fetch('http://localhost:5173/pokemon.json')
-		.then((res) => res.json())
-		.then((data) => setPokemon(data));
-	}, [filter])
+		fetch("http://localhost:5173/pokemon.json")
+			.then((res) => res.json())
+			.then((data) => setPokemon(data));
+	}, [filter]);
 
 	return (
 		<main>
-			<h1 className="title">Pokemon Search</h1>
+			<Container>
+			<Title>Pokemon Search</Title>
 
-			<div
-				style={{
-					display: "grid",
-					gridTemplateColumns: "70% 30%",
-					gridColumnGap: "1rem",
-				}}
-			>
-				<div>
-					<input
-						type="text"
-						placeholder="Search Name.."
-						value={filter}
-						onChange={(evt) => setFilter(evt.target.value)}
-					/>
+<TwoColumnsRow>
+	<div>
+		<Input
+			type="text"
+			placeholder="Search Name.."
+			value={filter}
+			onChange={(evt) => setFilter(evt.target.value)}
+		/>
 
-					<table width="100%">
-						<thead>
-							<tr>
-								<th>Name</th>
-								<th>Type</th>
-							</tr>
-						</thead>
-						<tbody>
-							{pokemon
-								.filter((pokemon) =>
-									pokemon.name.english
-										.toLocaleLowerCase()
-										.includes(filter.toLowerCase())
-								)
-								.slice(0, 20)
-								.map((pokemon) => (
-									<PokemonRow
-										key={[
-											pokemon.id,
-											pokemon.name.english,
-										].join(":")}
-										pokemon={pokemon}
-										onSelect={(pokemon) =>
-											setSelectedItem(pokemon)
-										}
-									/>
-								))}
-						</tbody>
-					</table>
-				</div>
+		<table width="100%">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+				</tr>
+			</thead>
+			<tbody>
+				{pokemon
+					.filter((pokemon) =>
+						pokemon.name.english
+							.toLocaleLowerCase()
+							.includes(filter.toLowerCase())
+					)
+					.slice(0, 20)
+					.map((pokemon) => (
+						<PokemonRow
+							key={[
+								pokemon.id,
+								pokemon.name.english,
+							].join(":")}
+							pokemon={pokemon}
+							onSelect={(pokemon) =>
+								setSelectedItem(pokemon)
+							}
+						/>
+					))}
+			</tbody>
+		</table>
+	</div>
 
-				<div>{selectedItem && <PokemonInfo {...selectedItem} />}</div>
-			</div>
+	<div>{selectedItem && <PokemonInfo {...selectedItem} />}</div>
+</TwoColumnsRow>
+			</Container>
 		</main>
 	);
 }
