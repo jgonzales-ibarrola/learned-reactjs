7f5e65e9bae6c517e4bf8e22f76294c3c8be0100
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-06 17:08:01 +0800

    style: Update container of the table and table

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ function App() {
     <main>
       <h1 className='title'>Pokemon Search</h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Bulbasaur</td>
-            <td>Grass, Poison</td>
-          </tr>
-        </tbody>
-      </table>
+     <div 
+      style={{
+        margin: 'auto',
+        width: 800,
+        paddingTop: '1rem'
+      }}
+     >
+      <table width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Bulbasaur</td>
+              <td>Grass, Poison</td>
+            </tr>
+          </tbody>
+        </table>
+     </div>
     </main>
   )
 }
